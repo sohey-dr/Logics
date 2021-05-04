25ae4f08f3f016592c522773ea1e390e38a06cd7
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-05-04 16:13:42 +0900

    Add express.json()

--- jsonOutPutter.js
+++ jsonOutPutter.js
@@ -1,20 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-});
+app.post('/', function (req, res) {
+  console.log(req.body);
+  res.send('Got a POST request')
+})
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
-
-const port = '8080';
-app.listen(port, () => {
-  console.log(`app start listening on port ${port}`);
-});
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
