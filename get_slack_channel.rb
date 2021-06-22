8422b6e7bd707c817e4670a203bc634595493669
sohey-dr <heyso1209@gmail.com>
2021-06-22 23:56:58 +0900

    Add get_slack_channel

---
+++ get_slack_channel.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'uri'
+require "json"
+require 'pp'
+
+TOKEN = ""
+SLACK_API_BASE = 'https://slack.com/api'
+
+
+fetch_all_channels_url = "https://slack.com/api/conversations.list?token=#{TOKEN}"
+res = Net::HTTP.get(URI.parse(fetch_all_channels_url))
+channel_hash = JSON.parse(res)
+channels = channel_hash["channels"]
+channels = channels.each { |channel| p channel["name"] } # 削除されたチャンネルや所属人数が0のチャンネルを除外
