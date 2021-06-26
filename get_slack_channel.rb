d58fa46fe376addc75038e84685cd534ca7667c6
sohey-dr <heyso1209@gmail.com>
2021-06-26 17:30:12 +0900

    Add next_cursor

--- get_slack_channel.rb
+++ get_slack_channel.rb
@@ -3,11 +3,14 @@ require 'uri'
 require "json"
 require 'pp'
 
-TOKEN = ""
+TOKEN = "xoxb-324037009922-958746922000-t28FH2rsSxB5tTYAWkpUGApN"
 SLACK_API_BASE = 'https://slack.com/api'
 
 fetch_all_channels_url = "https://slack.com/api/conversations.list?token=#{TOKEN}&types=public_channel,private_channel&limit=10"
 res = Net::HTTP.get(URI.parse(fetch_all_channels_url))
 channel_hash = JSON.parse(res)
 channels = channel_hash["channels"]
-channels = channels.each { |channel| p channel["name"] }
+channels = channels.each { |channel| puts channel["name"] }
+
+# 1000件までしか取得できないため、次の情報を以下の値から取得できる
+puts channel_hash["response_metadata"]["next_cursor"]
