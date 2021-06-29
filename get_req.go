81e68bac4ff8f745a2daad1371be8397d5e3dac3
sohey-dr <heyso1209@gmail.com>
2021-06-29 10:55:34 +0900

    Add get req

---
+++ get_req.go
@@ -0,0 +1,47 @@
+package main
+ 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+ 
+func main() {
+ 
+	url := "https://slack.com/api/conversations.list?types=public_channel,private_channel"
+ 
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil{
+		log.Fatal(err)
+	}
+	
+	//クエリパラメータ
+	// params := request.URL.Query()
+  //   params.Add("userId","1")
+  //   request.URL.RawQuery = params.Encode()
+	
+	// header設定
+	// request.Header.Set("Authorization", "Bearer TOKEN")
+
+
+	timeout := time.Duration(5 * time.Second)
+	client := &http.Client{
+        Timeout: timeout,
+	}
+ 
+	response, err := client.Do(request)
+	if err != nil{
+		log.Fatal(err)
+	}
+	
+	defer response.Body.Close()
+ 
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+ 
+	fmt.Println(string(body))
+}
