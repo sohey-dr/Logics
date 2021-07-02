8872daef05260cdbda36acfc5079f037931172ba
soheyHashimoto <heyso1209@gmail.com>
2021-07-02 18:43:28 +0900

    Update slack/conversations_invite.rb

--- slack/conversations_invite.rb
+++ slack/conversations_invite.rb
@@ -16,6 +16,5 @@ CSV.foreach('slack/channels.csv') do |row|
   if row[1] == "user_private"
     puts client.conversations_invite(channel: row[0], users: "UU6MYT400,U01GX8B06KH,U01D0EPHJ81,U020ZD483AR,U0204URQ6DR,U021MR7M5LP,U025JTP4Z4Z,U01TCCDBLTG,U01PNM7NA8J")
   end
-  sleep 1
+  # sleep 1
 end
-
