bcae92822817a6c3e67d7f4b78d942a254571dfb
sohey-dr <heyso1209@gmail.com>
2021-07-03 16:51:27 +0900

    Clean format

--- get_req.go
+++ get_req.go
@@ -1,5 +1,5 @@
 package main
- 
+
 import (
 	"fmt"
 	"io/ioutil"
@@ -7,41 +7,40 @@ import (
 	"net/http"
 	"time"
 )
- 
+
 func main() {
- 
+
 	url := "https://slack.com/api/conversations.list?types=public_channel,private_channel"
- 
+
 	request, err := http.NewRequest("GET", url, nil)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	//クエリパラメータ
 	// params := request.URL.Query()
-  //   params.Add("userId","1")
-  //   request.URL.RawQuery = params.Encode()
-	
+	//   params.Add("userId","1")
+	//   request.URL.RawQuery = params.Encode()
+
 	// header設定
 	// request.Header.Set("Authorization", "Bearer TOKEN")
 
-
 	timeout := time.Duration(5 * time.Second)
 	client := &http.Client{
-        Timeout: timeout,
+		Timeout: timeout,
 	}
- 
+
 	response, err := client.Do(request)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	defer response.Body.Close()
- 
+
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
- 
+
 	fmt.Println(string(body))
 }
