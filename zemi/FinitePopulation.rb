eaa6daf5677c4e1a270939485ac8e4841ea26c80
sohey-dr <heyso1209@gmail.com>
2021-07-08 17:28:45 +0900

    Add FinitePopulation

---
+++ zemi/FinitePopulation.rb
@@ -0,0 +1,29 @@
+=begin
+20210708にゼミでやった。
+母数に対して世論調査でどのくらいの人数調査すべきか出す。
+=end
+
+class FinitePopulation
+  def output
+    mothers / children
+  end
+
+  def mothers
+    16000
+  end
+
+  def child1
+    0.05 / 1.96
+  end
+
+  def child2
+    mother = mothers - 1
+    mother / 0.25
+  end
+
+  def children
+    child1 * child1 * child2 + 1
+  end
+end
+
+p FinitePopulation.new.output
