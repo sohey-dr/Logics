95ab551b1e40eae4bb1470e02ca872d3962c2c45
sohey-dr <heyso1209@gmail.com>
2021-07-10 01:17:51 +0900

    [ADD]Registerにuserの情報を渡す

--- laravel-admin-puppeteer/auto-register.js
+++ laravel-admin-puppeteer/auto-register.js
@@ -1,5 +1,9 @@
 (async() => {
   const Register = require("./libs/Register")
-  const register = new Register()
-  await register.run()
+  const CSVReader = require("./libs/CSVReader")
+  const users = CSVReader.run();
+  users.forEach(user => {
+    const register = new Register(user);
+    await register.run();
+  });
 })();
