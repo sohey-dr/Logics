79df08d4097f54172375403fd9c1b58c48373333
sohey-dr <heyso1209@gmail.com>
2021-07-10 01:10:59 +0900

    [ADD]CSVReader追加

--- laravel-admin-puppeteer/libs/CSVReader.js
+++ laravel-admin-puppeteer/libs/CSVReader.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
-const csv = require('csv');
+const csvSync = require('csv-parse/lib/sync');
 
-fs.createReadStream('laravel-admin-puppeteer/perfect_user.csv')
-  .pipe(csv.parse(function(err, data) {
- 
-        console.log(data);
- 
-  }));
+module.exports = class CSVReader {
+  constructor() {}
+
+  run() {
+    const users = this._readerCSV();
+    return users;
+  }
+
+  _readerCSV() {
+    const csv = fs.readFileSync('laravel-admin-puppeteer/perfect_user.csv');
+    return csvSync(csv);
+  }
+}
