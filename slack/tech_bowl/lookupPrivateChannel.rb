ec5cf6e401302ad70f466c2a08118c448c8a455b
sohey-dr <heyso1209@gmail.com>
2021-07-09 21:25:43 +0900

    [ADD]LookupPrivateChannel完成

--- slack/tech_bowl/lookupPrivateChannel.rb
+++ slack/tech_bowl/lookupPrivateChannel.rb
@@ -4,13 +4,31 @@ require "csv"
 チャンネル一覧取得
 
 =end
-users.each do |user|
-  user_name = user[5].gsub(" ", "")
-end
 
 class LookupPrivateChannel
-  def 
-    
+  def write_csv
+    CSV.open("slack/tech_bowl/perfect_user.csv", "w") do |csv|
+      set_users.each do |user|
+        csv << user
+      end
+    end
+  end
+
+  def set_users
+    users.each do |user|
+      user[6] = lookup_channel_by_user_name(user[5].gsub(" ", ""))
+    end
+  end
+
+  def lookup_channel_by_user_name(user_name)
+    channel_name = ""
+    channels.each do |channel|
+      if channel[3].include?(user_name)
+        channel_name = channel[1]
+        break
+      end
+    end
+    channel_name == '' ? nil : channel_name
   end
 
   def channels
@@ -21,3 +39,5 @@ class LookupPrivateChannel
     @users ||= CSV.read("slack/tech_bowl/users(registered_slack_id).csv")
   end
 end
+
+LookupPrivateChannel.new.write_csv
