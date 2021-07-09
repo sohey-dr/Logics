cd10598eca297f2502a5fa028c778ad9e0c7af28
sohey-dr <heyso1209@gmail.com>
2021-07-09 16:27:06 +0900

    [ADD]CSVの内容を置き換える処理追加

--- slack/tech_bowl/lookupByEmail.rb
+++ slack/tech_bowl/lookupByEmail.rb
@@ -1,9 +1,19 @@
 require 'slack-ruby-client'
+require "csv"
 
 Slack.configure do |conf|
   conf.token = ""
 end
 
 client = Slack::Web::Client.new
-user = client.users_lookupByEmail(email: "heyso1209@gmail.com").user
-user.id
+
+rows = CSV.read("slack/tech_bowl/users (1).csv")
+rows.each do |row|
+  row[7] = client.users_lookupByEmail(email: row[14]).user.id
+end
+
+CSV.open("slack/tech_bowl/users(registered_slack_id)", "w") do |csv|
+  rows.each do |row|
+    csv << row
+  end
+end
