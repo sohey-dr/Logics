8bc91246de4823120a61146d230c8b73d1f12537
sohey-dr <heyso1209@gmail.com>
2021-07-11 18:12:58 +0900

    [FIX]constructorを機能するようにする

--- laravel-admin-puppeteer/auto-register.js
+++ laravel-admin-puppeteer/auto-register.js
@@ -1,6 +1,8 @@
 (async() => {
   const Register = require("./libs/Register")
   const CSVReader = require("./libs/CSVReader")
+  const csvReader = new CSVReader
+  const users = csvReader.run();
   for await (const user of users) {
     const register = new Register(user);
     await register.run();
