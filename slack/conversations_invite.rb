6d6ceda7a745ebb84692d2738babf4ee613de6c6
sohey-dr <heyso1209@gmail.com>
2021-07-13 08:51:08 +0900

    [MODIFY]パスと指定カラムの変更

--- slack/conversations_invite.rb
+++ slack/conversations_invite.rb
@@ -8,13 +8,13 @@ Slack APIのconversations.invite(https://api.slack.com/methods/conversations.inv
 =end
 
 Slack.configure do |conf|
-  conf.token = ""
+  conf.token = "xoxp-324037009922-324037009986-2222883071830-9cb0c05866427228f477b5691c860aed"
 end
 
 client = Slack::Web::Client.new
-CSV.foreach('slack/channels.csv') do |row|
-  if row[1] == "user_private"
-    puts client.conversations_invite(channel: row[0], users: "UU6MYT400,U01GX8B06KH,U01D0EPHJ81,U020ZD483AR,U0204URQ6DR,U021MR7M5LP,U025JTP4Z4Z,U01TCCDBLTG,U01PNM7NA8J")
+CSV.foreach('slack/tech_bowl/channels.csv') do |row|
+  if row[2] == "mentor_private"
+    puts client.conversations_invite(channel: row[1], users: "UU6MYT400,U01GX8B06KH,U01D0EPHJ81,U020ZD483AR,U0204URQ6DR,U021MR7M5LP,U025JTP4Z4Z,U01TCCDBLTG,U01PNM7NA8J")
+    sleep 1
   end
-  # sleep 1
 end
