242c3d14d83eeb75d8db19def7fd1f5f25a9bbc4
soheyHashimoto <heyso1209@gmail.com>
2021-09-11 02:49:00 +0900

    [ADD]スクレイピングしたら10秒間スリープさせる

--- go/start_db/main.go
+++ go/start_db/main.go
@@ -1,12 +1,14 @@
 package main
  
 import (
+	"time"
 	"start_db/scraper"
 )
 
 func main() {
 	urls, _ := scraper.GetCatgoryUrls()
 	for _, url := range urls {
+		time.Sleep(time.Second * 10)
 		scraper.GetComUrlByList(url)	
 	}
 }
