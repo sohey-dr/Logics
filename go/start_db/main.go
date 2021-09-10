42a2b8db6365f7df6f7c2c2c7e164b48c255257c
soheyHashimoto <heyso1209@gmail.com>
2021-09-11 02:05:45 +0900

    [ADD]scraperリポジトリで分ける

--- go/start_db/main.go
+++ go/start_db/main.go
@@ -1,44 +1,10 @@
 package main
  
 import (
-	"fmt"
-	"log"
-	"net/http"
- 
-	"github.com/PuerkitoBio/goquery"
+	"start_db/scraper"
 )
 
 func main() {
-	// getCatgoryUrls()
-	getComUrlByList()
-}
-
-func getCatgoryUrls()  {
-	res, err := http.Get("https://startup-db.com/tags")
-	if err != nil {
-		log.Println(err)
-	}
-	defer res.Body.Close()
-
-	doc, _ := goquery.NewDocumentFromReader(res.Body)
-	doc.Find(".tag-children-ul > li > a").Each(func(i int, s *goquery.Selection) {
-		href, _ := s.Attr("href")
-		var next_url string = `"https://startup-db.com` + href + `",`
-		fmt.Println(next_url)
-	})
-}
-
-func getComUrlByList()  {
-	res, err := http.Get("https://startup-db.com/tags/cloud-funding")
-	if err != nil {
-		log.Println(err)
-	}
-	defer res.Body.Close()
-
-	doc, _ := goquery.NewDocumentFromReader(res.Body)
-	doc.Find(".CompanyCard > a").Each(func(i int, s *goquery.Selection) {
-		href, _ := s.Attr("href")
-		var next_url string = `"https://startup-db.com` + href + `",`
-		fmt.Println(next_url)
-	})
+	// scraper.GetCatgoryUrls()
+	scraper.GetComUrlByList()
 }
