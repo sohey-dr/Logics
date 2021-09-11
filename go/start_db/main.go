1b1c6226353c5ae46a068b249095255c59fb9802
soheyHashimoto <heyso1209@gmail.com>
2021-09-11 13:54:52 +0900

    [ADD]全ての企業詳細ページを吐き出せるようにする

--- go/start_db/main.go
+++ go/start_db/main.go
@@ -16,6 +16,18 @@ func main() {
 
 	for i, categoryUrl := range categoryUrls {
 		time.Sleep(time.Second * 10)
-		scraper.GetComUrlByList(url)	
+
+		fmt.Println("再開します")
+		companyUrlsInList, _ := scraper.GetComUrlByList(categoryUrl)
+
+		companyUrls = append(companyUrls, companyUrlsInList...)
 	}
+
+	var records [][]string
+	for i, companyUrl := range companyUrls {
+		record := []string{strconv.Itoa(i), companyUrl, "2021-09-10 14:38:54", "2021-09-10 14:38:54"}
+		records = append(records, record)
+	}
+
+	csv.Write(records)
 }
