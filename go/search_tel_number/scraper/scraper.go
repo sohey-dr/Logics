738d9d38f830668e3b63b7bcbe891dc95c92afaf
soheyHashimoto <heyso1209@gmail.com>
2021-09-12 18:26:11 +0900

    [ADD]郵便番号と一致する文字列がspanで見つかったかをbooleanで管理

--- go/search_tel_number/scraper/scraper.go
+++ go/search_tel_number/scraper/scraper.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -17,11 +18,17 @@ func SearchTelNumber(url, address string) {
 	defer res.Body.Close()
 
 	fmt.Println("探しましょう")
+	existsAddress := false
 	doc, _ := goquery.NewDocumentFromReader(res.Body)
 
 	// NOTE: 要素が変わることがあるため広いspanタグで指定している
 	doc.Find("span").Each(func(i int, s *goquery.Selection) {
 		text := s.Text()
-		fmt.Println(text)
+
+		// NOTE: 郵便番号と一致する文字列がspanで見つかったらexistsAddressがtrueを返す
+		// ex. address == "〒600-8118 京都府京都市下京区平居町５８番地 本池中 UNKNOWN"
+		if strings.HasPrefix(text, address[:11]) {
+			existsAddress = true
+		}
 	})
 }
