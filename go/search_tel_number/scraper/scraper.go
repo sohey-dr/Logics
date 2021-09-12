7c58357cd85f92c3eb9bcb1a8d9f4ec25ab6146d
soheyHashimoto <heyso1209@gmail.com>
2021-09-12 13:53:42 +0900

    [ADD]スクレイパー実装

---
+++ go/search_tel_number/scraper/scraper.go
@@ -0,0 +1,25 @@
+package scraper
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// SearchTelNumber Googleで「{企業名} 電話番号」で検索して出てきたものが同一の企業と判断できた場合にmapを返す
+func SearchTelNumber(url string) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	defer res.Body.Close()
+
+	fmt.Println("探しましょう")
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+	doc.Find("span").Each(func(i int, s *goquery.Selection) {
+		text := s.Text()
+		fmt.Println(text)
+	})
+}
