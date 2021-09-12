23f0214914432aa07bde8046ef159e4392bb584e
soheyHashimoto <heyso1209@gmail.com>
2021-09-12 18:25:10 +0900

    [ADD]addressは変数で使うため定義

--- go/search_tel_number/main.go
+++ go/search_tel_number/main.go
@@ -3,5 +3,7 @@ package main
 import "search_tel_number/scraper"
 
 func main() {
-	scraper.SearchTelNumber("https://www.google.com/search?q=株式会社CredoShip.", "")
+	address := "〒600-8118 京都府京都市下京区平居町５８番地 本池中 UNKNOWN"
+
+	scraper.SearchTelNumber("https://www.google.com/search?q=株式会社CredoShip.", address)
 }
