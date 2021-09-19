aa11fbb531eae92bba356a2e2452f897c8db8014
soheyHashimoto <heyso1209@gmail.com>
2021-09-19 23:49:39 +0900

    [ADD]直接contributionsの文字列を返すようにする

--- go/get-github-contributions/scraper/scraper.go
+++ go/get-github-contributions/scraper/scraper.go
@@ -9,14 +9,14 @@ import (
 )
 
 // GetContributions 
-func GetContributions(userName string) int {
+func GetContributions(userName string) string {
 	url := "https://github.com/" + userName
 	res, err := http.Get(url)
 	if err != nil {
 		log.Println(err)
 	}
 
-	var contributions int
+	var contributeNum string
 
 	doc, _ := goquery.NewDocumentFromReader(res.Body)
 	// NOTE: 要素が変わることがあるため広いspanタグで指定している
@@ -26,5 +26,5 @@ func GetContributions(userName string) int {
 		contributions, _ = strconv.Atoi(contributeNum)
 	})
 
-	return contributions
+	return contributeNum
 }
