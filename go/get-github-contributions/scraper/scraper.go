c2ae1447dcd6294cc43b8045bc0fa45913aea899
soheyHashimoto <heyso1209@gmail.com>
2021-09-20 00:18:26 +0900

    [MODIFY]文字列操作を他のpackageにしたことによる変更

--- go/get-github-contributions/scraper/scraper.go
+++ go/get-github-contributions/scraper/scraper.go
@@ -3,27 +3,26 @@ package scraper
 import (
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
+
+	"get-github-contributions/strings"
 )
 
 // GetContributions 
-func GetContributions(userName string) string {
+func GetContributions(userName string) int64 {
 	url := "https://github.com/" + userName
 	res, err := http.Get(url)
 	if err != nil {
 		log.Println(err)
 	}
 
-	var contributeNum string
+	var contributeNum int64
 
 	doc, _ := goquery.NewDocumentFromReader(res.Body)
 	// NOTE: 要素が変わることがあるため広いspanタグで指定している
-	doc.Find("span").Each(func(i int, s *goquery.Selection) {
-		contributeNum := s.Text()
-		
-		contributions, _ = strconv.Atoi(contributeNum)
+	doc.Find(".position-relative > h2").Each(func(i int, s *goquery.Selection) {
+		contributeNum = strings.FindNum(s.Text())
 	})
 
 	return contributeNum
