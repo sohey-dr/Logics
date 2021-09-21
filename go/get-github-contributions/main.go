6128ff3542ebb0435e3402c30b53d350256c69de
soheyHashimoto <heyso1209@gmail.com>
2021-09-22 02:51:41 +0900

    [ADD]userのmapにcontribute数を保持するようにする

--- go/get-github-contributions/main.go
+++ go/get-github-contributions/main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"get-github-contributions/csv"
 	"get-github-contributions/scraper"
+	"log"
+	"strconv"
 )
 
 func main() {
 	users := csv.GetGithubId()
+
 	for _, user := range users {
-		contributeNum := scraper.GetContributions(user["githubId"])
-		fmt.Println(contributeNum)
+		contributeNum, err := scraper.GetContributions(user["githubId"])
+		if err != nil {
+			log.Println(err)
+			user["contributeNum"] = ""
+		} else {
+			user["contributeNum"] = strconv.FormatInt(contributeNum, 10)
+		}
+
+		fmt.Println(user)
 	}
 }
