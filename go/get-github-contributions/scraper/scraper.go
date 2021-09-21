b8b0754b9fcc76ff1d5964b82a22e27b387bd413
soheyHashimoto <heyso1209@gmail.com>
2021-09-21 21:15:34 +0900

    [MODIFY]スクレイピングの失敗がわかるようにする

--- go/get-github-contributions/scraper/scraper.go
+++ go/get-github-contributions/scraper/scraper.go
@@ -3,6 +3,7 @@ package scraper
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 
@@ -10,19 +11,23 @@ import (
 )
 
 // GetContributions contribute数をスクレイピングして返す
-func GetContributions(userName string) int64 {
+func GetContributions(userName string) int {
 	url := "https://github.com/" + userName
 	res, err := http.Get(url)
 	if err != nil {
 		log.Println(err)
 	}
 
-	var contributeNum int64
+	// NOTE: -1はありえないので取得した時にスクレイピングできなかったことをわかるようにしている
+	contributeNum := -1
 
 	doc, _ := goquery.NewDocumentFromReader(res.Body)
 	// NOTE: 要素が変わることがあるため広いspanタグで指定している
 	doc.Find(".position-relative > h2").Each(func(i int, s *goquery.Selection) {
-		contributeNum = strings.FindNum(s.Text())
+		contributeNum, err = strconv.Atoi(s.Text())
+		if err != nil {
+			log.Println("scraping err")
+		}
 	})
 
 	return contributeNum
