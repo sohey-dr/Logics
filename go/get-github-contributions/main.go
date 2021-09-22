4e2b76cacb3c4671bef5acdfc0dc25bc5049ab43
soheyHashimoto <heyso1209@gmail.com>
2021-09-22 23:57:27 +0900

    [ADD]contributes数を付け加えてCSVに書き込むようにする

--- go/get-github-contributions/main.go
+++ go/get-github-contributions/main.go
@@ -20,6 +20,14 @@ func main() {
 		} else {
 			user["contributeNum"] = strconv.FormatInt(contributeNum, 10)
 		}
+		record := []string{
+			user["id"],
+			user["name"],
+			user["githubId"],
+			user["contributeNum"],
+		}
+
+		csv.WriteUserContributes(record)
 
 		time.Sleep(2 * time.Second)
 		fmt.Println(user)
