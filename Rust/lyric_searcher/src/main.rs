5a994750586c21b81365ba8df0f1eab26e5d1abf
soheyHashimoto <heyso1209@gmail.com>
2021-10-17 20:36:59 +0900

    [ADD]スクレイピングスクリプト実装

--- Rust/lyric_searcher/src/main.rs
+++ Rust/lyric_searcher/src/main.rs
@@ -1,3 +1,15 @@
+use scraper::{Html, Selector};
+
 fn main() {
-    println!("Hello, world!");
+    let body  = reqwest::blocking::get("https://blog.rust-lang.org/")?.text()?;
+
+    // HTMLをパース
+    let document = scraper::Html::parse_document(&body);
+
+    let fragment = Html::parse_fragment(document);
+    let selector = Selector::parse("li").unwrap();
+
+    for element in fragment.select(&selector) {
+        assert_eq!("li", element.value().name());
+    }
 }
