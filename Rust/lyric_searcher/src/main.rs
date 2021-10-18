5288711a054f663a3658f860e691213db1a3500f
soheyHashimoto <heyso1209@gmail.com>
2021-10-18 13:09:33 +0900

    [ADD]解析してほしい値を取得

--- Rust/lyric_searcher/src/main.rs
+++ Rust/lyric_searcher/src/main.rs
@@ -1,17 +1,16 @@
 use scraper::{Html, Selector};
 
 fn main() -> eyre::Result<()>{
-    let body  = reqwest::blocking::get("https://blog.rust-lang.org/")?.text()?;
+    let body = reqwest::blocking::get("https://www.google.com/search?q=%E5%90%9B%E3%81%8C%E5%A5%BD%E3%81%8D+%E6%AD%8C%E8%A9%9E")?.text()?;
 
-    let selector = Selector::parse("td.bn > a").unwrap();
-
+    let selector = Selector::parse("a").unwrap();
 
     let document = Html::parse_document(&body);
 
-    let elements = document.select(&selector);
-
-    // 全記事名を出力
-    elements.for_each(|e| println!("{}", e.text().next().unwrap()));
+    for node in document.select(&selector) {
+        println!("{:?}", node.inner_html());
+        println!("{:?}", node.value().attr("href"));
+    }
 
     Ok(())
 }
