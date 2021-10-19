7950744ab894a501bb8a6a2b9abc99d0aa2c1671
soheyHashimoto <heyso1209@gmail.com>
2021-10-19 23:05:24 +0900

    [ADD]hrefからurlのみを抽出

--- Rust/lyric_searcher/src/main.rs
+++ Rust/lyric_searcher/src/main.rs
@@ -2,17 +2,18 @@ use scraper::{Html, Selector};
 
 fn main() -> eyre::Result<()>{
     let body = reqwest::blocking::get("https://www.google.com/search?q=%E5%90%9B%E3%81%8C%E5%A5%BD%E3%81%8D+%E6%AD%8C%E8%A9%9E")?.text()?;
-
-    let selector = Selector::parse(".kCrYT > a").unwrap();
-
     let document = Html::parse_document(&body);
+    let selector = Selector::parse(".kCrYT > a").unwrap();
 
     for node in document.select(&selector) {
         let href = node.value().attr("href").unwrap();
 
-        if href.contains("uta-net") || href.contains("j-lyric.net") || href.contains("utamap") {
+        let last_index = href.find("&sa=").unwrap();
+        let url = &href[7..last_index];
+
+        if url.contains("uta-net") || url.contains("j-lyric.net") || url.contains("utamap") {
             println!("{:?}", node.text().collect::<Vec<_>>()[0]);
-            println!("{:?}", href);
+            println!("{:?}", url);
         }
     }
 
