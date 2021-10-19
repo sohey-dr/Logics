625572535b3fbaee6e58e556fd8a478d27e9cece
soheyHashimoto <heyso1209@gmail.com>
2021-10-19 19:53:55 +0900

    [ADD]ヘッドレス時のGoogleのhtmlを表示

--- test.rb
+++ test.rb
@@ -1,13 +1,11 @@
 require "net/http"
 
-uri = URI.parse("https://k7qropickw.summer2021.dena.jp/channels/6834/messages")
+uri = URI.parse("https://www.google.com/search?q=%E5%90%9B%E3%81%8C%E5%A5%BD%E3%81%8D+%E6%AD%8C%E8%A9%9E")
 response = Net::HTTP.get_response(uri)
 
 response.code # status code
 body = response.body
-i = 0
 
-body.each do |b|
-  i += 1
-  b
-end
+File.open("result.html", mode = "w"){|f|
+  f.write(body)
+}
