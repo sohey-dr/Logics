990744978c8836fd796aaa650abaaad27b9bbf47
soheyHashimoto <heyso1209@gmail.com>
2021-10-19 20:41:43 +0900

    [ADD]検索結果のnodeを検索

--- Rust/lyric_searcher/src/main.rs
+++ Rust/lyric_searcher/src/main.rs
@@ -3,7 +3,7 @@ use scraper::{Html, Selector};
 fn main() -> eyre::Result<()>{
     let body = reqwest::blocking::get("https://www.google.com/search?q=%E5%90%9B%E3%81%8C%E5%A5%BD%E3%81%8D+%E6%AD%8C%E8%A9%9E")?.text()?;
 
-    let selector = Selector::parse("a").unwrap();
+    let selector = Selector::parse(".kCrYT > a").unwrap();
 
     let document = Html::parse_document(&body);
 
