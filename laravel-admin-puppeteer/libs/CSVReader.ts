28799b27f47aea9dea5f14997e2ea5324cc971e9
soheyHashimoto <heyso1209@gmail.com>
2021-10-31 09:42:37 +0900

    [ADD]型定義をする

--- laravel-admin-puppeteer/libs/CSVReader.ts
+++ laravel-admin-puppeteer/libs/CSVReader.ts
@@ -5,13 +5,12 @@ module.exports = class CSVReader {
   constructor() {}
 
   run() {
-    const users = this._readerCSV();
+    const users: Array<Array<string>> = this._readerCSV();
     return users;
   }
 
   _readerCSV() {
-    // csvのカラムごとに分けて多次元配列として返す
-    const csv = fs.readFileSync("./perfect_user.csv");
+    const csv: Array<Array<string>> = fs.readFileSync("./perfect_user.csv");
     return csvSync(csv);
   }
 };
