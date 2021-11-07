10cf00fd20a8f885f149bc86e2a7abffe55fcaa1
soheyHashimoto <heyso1209@gmail.com>
2021-11-07 17:31:32 +0900

    [ADD]alertを出力するアプリ

--- Rust/is_expression_valid/src/lib.rs
+++ Rust/is_expression_valid/src/lib.rs
@@ -1,3 +1,15 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn is_expression_valid(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
