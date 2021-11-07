7f3eb59dbcff492e4e6f99bf2af6ecfced0fd31c
soheyHashimoto <heyso1209@gmail.com>
2021-11-07 18:11:05 +0900

    [ADD]is_match_expression_validで不適切な表現かを判断する

--- Rust/is_expression_valid/src/lib.rs
+++ Rust/is_expression_valid/src/lib.rs
@@ -7,16 +7,33 @@ extern {
 
 #[wasm_bindgen]
 pub fn is_expression_valid(word: &str) {
-    if word == "" {
-        alert("不適切な値です");
+    if is_match_expression_valid(word) {
+        let alert_word = "{} は不適切な表現です".to_string() + word;
+        alert(&alert_word);
         return;
     }
+}
+
+fn is_match_expression_valid(word: &str) -> bool {
+    let invalid_words = vec!["うんち", "うんこ", "おしっこ"];
+    for iw in invalid_words {
+        if iw.contains(word) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn is_match_expression_valid() {
+        assert_eq!(super::is_match_expression_valid("うんち"), false);
+    }
+
+    #[test]
+    fn is_not_match_expression_valid() {
+        assert_eq!(super::is_match_expression_valid("きれい"), false);
     }
 }
