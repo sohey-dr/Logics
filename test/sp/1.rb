dde73ef3d7bcf0216ad40e356dca54dd02d9b530
soheyHashimoto <heyso1209@gmail.com>
2021-11-29 23:47:52 +0900

    [ADD]README更新

--- test/sp/1.rb
+++
@@ -1,9 +0,0 @@
-=begin
-あなたは正の整数の掛け算のみをする計算機のプログラムを作成しています。 
-その計算機は最大で4桁までしか表示できず 9,999 より大きな数字は表示が出来ず "NG" と表示されます。 
-2つの正の整数 a, b が入力されるので、a と bを掛け算した時 9,999 以下であれば掛け算した結果を出力し、10,000 以上の場合は "NG" と出力するプログラムを作成してください。 入力は空白で区切った文字列として標準入力から与えられます。 入力例: ``` 12 48 ```
-=end
-
-num_array = gets.chomp.split.map(&:to_i) 
-calculation_answer = num_array[0] * num_array[1] 
-puts calculation_answer < 10000 ? calculation_answer : "NG"
--- test/tl/2.rb
+++
@@ -1,5 +0,0 @@
-def divisor_sum(num, limit)
-  (1..limit).select { |i| num % i == 0 }.sum
-end
-
-puts divisor_sum(1234567890, 2000000)
