fa3593cd5cd0df33ee4fc53599f49cb6a9abbaad
soheyHashimoto <heyso1209@gmail.com>
2022-02-04 22:44:57 +0900

    [ADD]todoを返すようにする

--- go/graph_ql_server/graph/schema.resolvers.go
+++ go/graph_ql_server/graph/schema.resolvers.go
@@ -6,16 +6,23 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"graph_ql_server/graph/generated"
 	"graph_ql_server/graph/model"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	todo := &model.Todo{
+		Text:   input.Text,
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
