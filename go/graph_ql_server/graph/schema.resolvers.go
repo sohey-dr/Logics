9b8d9cb43053f516065ea0cd4bbc814e57a0207f
soheyHashimoto <heyso1209@gmail.com>
2022-02-04 22:56:50 +0900

    [ADD]Userの扱いを修正

--- go/graph_ql_server/graph/schema.resolvers.go
+++ go/graph_ql_server/graph/schema.resolvers.go
@@ -6,23 +6,27 @@ package graph
 import (
 	"context"
 	"fmt"
-	"math/rand"
 	"graph_ql_server/graph/generated"
 	"graph_ql_server/graph/model"
+	"math/rand"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
 	todo := &model.Todo{
 		Text:   input.Text,
 		ID:     fmt.Sprintf("T%d", rand.Int()),
-		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+		UserID: input.UserID, // fix this line
 	}
 	r.todos = append(r.todos, todo)
 	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	return r.todos, nil
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
+	return &model.User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -31,5 +35,9 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+// Todo returns generated.TodoResolver implementation.
+func (r *Resolver) Todo() generated.TodoResolver { return &todoResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type todoResolver struct{ *Resolver }
