3079eb3ff298c45b83f01b70f76603430cd66bae
soheyHashimoto <heyso1209@gmail.com>
2022-02-04 22:43:29 +0900

    [ADD]状態を追跡する場所を用意

--- go/graph_ql_server/graph/resolver.go
+++ go/graph_ql_server/graph/resolver.go
@@ -1,7 +1,9 @@
 package graph
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
+import (
+	"graph_ql_server/graph/model"
+)
 
-type Resolver struct{}
+type Resolver struct{
+	todos []*model.Todo
+}
