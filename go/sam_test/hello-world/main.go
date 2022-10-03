a5b596ec16cffe265ea036410276bcf3a148d151
soheyHashimoto <heyso1209@gmail.com>
2022-10-03 19:32:01 +0900

    [ADD]Authorizationも返す

--- go/sam_test/hello-world/main.go
+++ go/sam_test/hello-world/main.go
@@ -41,7 +41,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		Body:       fmt.Sprintf("Hello, %v , Authorization: %v", string(ip), request.Headers["Authorization"]),
 		StatusCode: 200,
 	}, nil
 }
