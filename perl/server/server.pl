83fce9a5789d6486930aed285e3527cd442f17fe
sohey-dr <heyso1209@gmail.com>
2023-07-02 23:24:43 +0900

    feat: jsonを返すrouteの追加

--- perl/server/server.pl
+++ perl/server/server.pl
@@ -6,5 +6,10 @@ use Mojolicious::Lite;
 use Mojo::JSON qw(decode_json encode_json);
 
 get '/' => sub { shift->render(text => 'Hello World!') };
+get '/json' => sub {
+	my $self = shift;
+	my $json = { name => 'mojo', version => '1.0' };
+	$self->render(json => $json);
+};
 
 app->start;
