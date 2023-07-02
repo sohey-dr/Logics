cae07aacb88da5d81e357338f8a07f0572ee64c5
sohey-dr <heyso1209@gmail.com>
2023-07-02 16:50:56 +0900

    feat: サブルーチン化

--- perl/hello/fizzbuzz.pl
+++ perl/hello/fizzbuzz.pl
@@ -1,17 +1,20 @@
 use strict;
 use warnings;
 
-my $i = 1;
+fizzbuzz();
 
-while ($i <= 100) {
-  if ($i % 15 == 0) {
-    print "FizzBuzz\n";
-  } elsif ($i % 3 == 0) {
-    print "Fizz\n";
-  } elsif ($i % 5 == 0) {
-    print "Buzz\n";
-  } else {
-    print "$i\n";
+sub fizzbuzz {
+  my $i = 1;
+  while ($i <= 100) {
+    if ($i % 15 == 0) {
+      print "FizzBuzz\n";
+    } elsif ($i % 3 == 0) {
+      print "Fizz\n";
+    } elsif ($i % 5 == 0) {
+      print "Buzz\n";
+    } else {
+      print "$i\n";
+    }
+    $i++;
   }
-  $i++;
 }
